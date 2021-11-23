91d44857632189b380cf981940c3fb7a493f42ed
rafidreezwan <69665294+rafidreezwan@users.noreply.github.com>
2021-11-23 14:22:29 +0600

    Add files via upload

---
+++ addNumber.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int addnmbr(long int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        cout<<i<<" ";
+    }
+    }
+int main(){
+long int n;
+cin>>n;
+cout<<addnmbr(n);
+return 0;
+}
+
